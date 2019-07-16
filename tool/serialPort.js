6e7bb75dde255506a52051eb5b7d5f41e6ec63b3
jack <361164217@qq.com>
2019-07-16 15:29:45 +0800

    给设备收到的property参数增加可视化效果

--- tool/serialPort.js
+++ tool/serialPort.js
@@ -1,20 +1,22 @@
-const fs = require('fs')
-const SerialPort = require('serialport')
-const color = require('colors-cli')
-require('colors-cli/toxic')
-const time = require('./time')
-const logContent = require('./log')
+const fs = require("fs")
+const SerialPort = require("serialport")
+const color = require("colors-cli")
+require("colors-cli/toxic")
+const chalk = require("chalk")
+const time = require("./time")
+const logContent = require("./log")
+var kelvinToRgb = require('kelvin-to-rgb')
 
-let logLineCache = ''
+let logLineCache = ""
 
 const onData = data => {
-    let dataString = data.toString('binary')
+    let dataString = data.toString("binary")
     logLineCache += dataString
     if (!/\n$/.test(dataString)) {
         return
     }
     printLog(logLineCache)
-    logLineCache = ''
+    logLineCache = ""
 }
 
 const printLog = log => {
@@ -22,51 +24,35 @@ const printLog = log => {
     const ignoreOriginalDeviceLogTag = true
     const displayTypeTag = true
     const displayCurrentTime = true
-    const displayUnknowTagLog = true // 显示未知、未预先定义的日志
+    const displayUnknowTagLog = false // 显示未知、未预先定义的日志
 
     // 计算缓存变量
     let unknowTag = false
     let tag = null
 
-    switch(true) {
-        case /Property\sSet\sReceived/ig.test(log): // 固件收到 APP 下发的指令
-        case /\@fromCloud/ig.test(log): // 固件收到 APP 下发的指令
-            log = log.green_bt
-            break
-        case /Message Post Reply Received/ig.test(log): // 固件上报数据给云端完成
-        case /\@sendToCloud/ig.test(log): // 固件上报数据给云端完成
-            log = log.yellow
-            break    
-        case /Property post all/ig.test(log): // 全部数据上报（每分钟 1 次）
-            log = log.yellow
-            break
-        default:
-            log = log.black_bt
-            break
-    }
 
     if (ignoreOriginalDeviceLogTag) {
         // log = log.replace(/\[\w+\]\[[\w\:\s]+\]/ig, '')
-        log = log.replace(/\[\w+\]/ig, '') // 保留固件代码行数打印
+        log = log.replace(/\[\w+\]/ig, "") // 保留固件代码行数打印
     }
 
     if (displayTypeTag) {
         // let tag = 'No Tag'
-        switch(true) {
+        switch (true) {
             case /Property\sSet\sReceived/ig.test(log): // 固件收到 APP 下发的指令
             case /\@fromCloud/ig.test(log): // 固件收到 APP 下发的指令
-                tag = 'From APP'
+                tag = "From APP"
                 break
             case /Message Post Reply Received/ig.test(log): // 固件上报数据给云端完成
             case /\@sendToCloud/ig.test(log): // 固件上报数据给云端完成
-                tag = 'To Cloud'
+                tag = "To Cloud"
                 break
             case /Property post all/ig.test(log): // 全部数据上报（每分钟 1 次）
-                tag = 'Post all'
+                tag = "Post all"
                 break
             default:
                 unknowTag = true
-                tag = 'Unknow'
+                tag = "Unknow"
                 break
         }
         // const tagMaxLength = 12 // 预定义 tag 字符串最大长度
@@ -89,19 +75,68 @@ const printLog = log => {
     //     return
     // }
 
+
+    let json
+    let property
+    if (/Property Set Received/ig.test(log)) {
+        json = log.trim().replace("[solo: 170] Property Set Received, Devid: 0, Request: ", "").replace(/[\r\n]/g, "")
+        property = JSON.parse((json))
+    }
+
+
+    // 最后再加颜色，避免给string前后加转义符
+    switch (true) {
+        case /Property Set Received/ig.test(log): // 固件收到 APP 下发的指令
+        case /\@fromCloud/ig.test(log): // 固件收到 APP 下发的指令
+            log = log.green_bt
+            break
+        case /Message Post Reply Received/ig.test(log): // 固件上报数据给云端完成
+        case /\@sendToCloud/ig.test(log): // 固件上报数据给云端完成
+            log = log.green_bt
+            break
+        case /Property post all/ig.test(log): // 全部数据上报（每分钟 1 次）
+            log = log.yellow
+            break
+        default:
+            log = log.black_bt
+            break
+    }
+
+
     // 去除行尾换行
     log = log
-        .replace(/^[\n\r\s]+/ig, '')
-        .replace(/[\n\r\s]+$/ig, '')
+        .replace(/^[\n\r\s]+/ig, "")
+        .replace(/[\n\r\s]+$/ig, "")
 
-    logContent(log, ['Firmware', tag])
-    // console.log(log)
+    logContent(log, ["Firmware", tag])
+
+
+    //property相关参数可视化
+
+    if (property) {
+        let status = property["LightSwitch"]
+        let bright = property["Brightness"]
+        let hsvColor = property["HSVColor"]
+        let temperature = property["ColorTemperature"]
+
+        let switchColor = status ? "white" : "black"
+        let brightString = "☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀☀".slice(0, parseInt(bright * 0.2)) + "                    ".slice(0, parseInt(20 - (bright * 0.2)))
+        let RGB=kelvinToRgb(temperature)
+        console.log('----------------------------------')
+        status >= 0 && console.log(chalk.blue.bold("开关"), chalk[switchColor]("████████████████████"), chalk.blue.bold(status))
+        bright && console.log(chalk.blue.bold("亮度"), chalk.yellow(brightString), chalk.blue.bold(bright))
+        temperature && console.log(chalk.blue.bold("色温"), chalk.rgb(...RGB)("████████████████████"), chalk.blue.bold(temperature))
+        hsvColor && console.log(chalk.blue.bold("颜色"), chalk.hsv(hsvColor["Hue"], hsvColor["Saturation"], hsvColor["Value"])("████████████████████"), chalk.blue.bold(hsvColor["Hue"]))
+        console.log('----------------------------------')
+
+    }
+
 }
 
 const serialPort = {
     port: null,
     get list() {
-        const fileList = fs.readdirSync('/dev', {encoding: 'utf8'})
+        const fileList = fs.readdirSync("/dev", {encoding: "utf8"})
             .filter(fileName => /^cu\./ig.test(fileName))
             .filter(fileName => !/^cu\.Bluetooth/ig.test(fileName))
             .filter(fileName => !/SPPDev/ig.test(fileName))
@@ -110,25 +145,25 @@ const serialPort = {
     baudRateList: [
         {
             value: 115200,
-            name: '115200 - MXCHIP 3080 / MOC 1290',
+            name: "115200 - MXCHIP 3080 / MOC 1290",
         },
         {
             value: 921600,
-            name: '921600 - MXCHIP 3060 / MOC 108',
+            name: "921600 - MXCHIP 3060 / MOC 108",
         },
     ],
     connect: (portPath, baudRate) => {
         this.port = new SerialPort(portPath, {
             baudRate,
         })
-        this.port.on('open', function() {
+        this.port.on("open", function () {
             console.log(`Has been open port ${portPath} by baudRate ${baudRate}`)
         })
-        this.port.on('readable', () => {
+        this.port.on("readable", () => {
             this.port.read()
         })
         // Switches the port into "flowing mode"
-        this.port.on('data', onData)
+        this.port.on("data", onData)
     },
 }
 
